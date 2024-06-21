48912b7dd0feef46aa6d543becb488481ce41afa
lastoflost <lastoflost@gmail.com>
2024-06-21 10:21:13 +0700

    first command

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Api;
+
+class TelegramController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected Api $telegram;
+
+    public function __construct(Api $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+    public function handleWebhook(Request $request): JsonResponse
+    {
+        Log::info("request", ['message' => $this->telegram->getWebhookUpdate()->getMessage()->text]);
+
+        $this->telegram->commandsHandler(true);
+
+        return response()->json(['status' => 'ok']);
+    }
+}
