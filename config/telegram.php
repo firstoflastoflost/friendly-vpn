0b073a00444506fc4be785703b5ad0bfd87d3af6
lastoflost <lastoflost@gmail.com>
2024-06-26 02:56:20 +0700

    fix start command

--- config/telegram.php
+++ config/telegram.php
@@ -36,7 +36,7 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                //Acme\Project\Commands\MyTelegramBot\BotCommand::class
+                StartCommand::class,
             ],
         ],
 
@@ -114,7 +114,6 @@ return [
     */
     'commands'                     => [
         Telegram\Bot\Commands\HelpCommand::class,
-        StartCommand::class,
     ],
 
     /*
